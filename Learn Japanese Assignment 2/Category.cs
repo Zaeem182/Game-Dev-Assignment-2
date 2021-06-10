a83806da867edc3e7c069267c496c38342181152
Zaeem Petersen <zaeempetersen@gmail.com>
2021-06-10 18:07:29 +0200

    category form basics

---
+++ Learn Japanese Assignment 2/Category.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Learn_Japanese_Assignment_2
+{
+    public partial class Category : Form
+    {
+        public Category()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Category_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NounCategoryBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
