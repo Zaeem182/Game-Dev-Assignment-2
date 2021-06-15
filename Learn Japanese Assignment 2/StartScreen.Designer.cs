f1cca02053b46f6a3fed694f71baf66cb2db31e7
Zelino Pestana-218325991 <zelinopestana@gmail.com>
2021-06-15 14:38:42 +0200

    new pictures

--- Learn Japanese Assignment 2/StartScreen.Designer.cs
+++ Learn Japanese Assignment 2/StartScreen.Designer.cs
@@ -29,27 +29,28 @@ namespace Learn_Japanese_Assignment_2
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartScreen));
             this.btb1 = new System.Windows.Forms.Button();
             this.btn2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btb1
             // 
+            this.btb1.Image = ((System.Drawing.Image)(resources.GetObject("btb1.Image")));
             this.btb1.Location = new System.Drawing.Point(12, 24);
             this.btb1.Name = "btb1";
-            this.btb1.Size = new System.Drawing.Size(454, 525);
+            this.btb1.Size = new System.Drawing.Size(466, 525);
             this.btb1.TabIndex = 0;
-            this.btb1.Text = "Tutorial";
             this.btb1.UseVisualStyleBackColor = true;
             this.btb1.Click += new System.EventHandler(this.btb1_Click);
             // 
             // btn2
             // 
+            this.btn2.Image = ((System.Drawing.Image)(resources.GetObject("btn2.Image")));
             this.btn2.Location = new System.Drawing.Point(514, 24);
             this.btn2.Name = "btn2";
             this.btn2.Size = new System.Drawing.Size(446, 525);
             this.btn2.TabIndex = 1;
-            this.btn2.Text = "Start Learning";
             this.btn2.UseVisualStyleBackColor = true;
             this.btn2.Click += new System.EventHandler(this.btn2_Click);
             // 
